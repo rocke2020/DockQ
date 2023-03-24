44d6fbe505953b5852a0a63c5db1364417898c4e
qichang.dong <qichang.dong@videt.cn>
2023-03-24 16:42:06 +0800

    u

---
+++ app.py
@@ -0,0 +1,12 @@
+import re, random
+from pathlib import Path
+import json
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+import os, sys
+
+
+def cp_test_files(pdb_id_chains):
+    """  """
+    pass
